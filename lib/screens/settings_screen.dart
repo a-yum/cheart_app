be7958a52c6fcbbebe8b526db5dfe8157c696eaf
a-yum <yum.andrew15@gmail.com>
2025-03-28 12:05:45 -0500

    placeholder screens

---
+++ lib/screens/settings_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import '/themes/cheart_theme.dart';
+import '/components/bottom_navbar.dart';
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+        backgroundColor: CHeartTheme.primaryColor,
+      ),
+      body: const Center(
+        child: Text(
+          'Settings Screen\nPlaceholder',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavbar(
+        currentIndex: 3,
+        onTap: (index) {
+          // TODO: Navigation logic will be implemented when global state is added
+        },
+      ),
+    );
+  }
+}
